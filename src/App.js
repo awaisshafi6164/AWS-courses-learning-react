3a5354a013aaa573570f350c75b726c0adea6e2f
MAwais <awais_shafi@elixir.com>
2024-08-28 14:51:12 +0500

    Initial commit to the repo.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React For AWS Serverless programming
         </a>
       </header>
     </div>
